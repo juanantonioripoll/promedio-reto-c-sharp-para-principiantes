00db59aa42548adbb307dec75a3a56b481a800fb
anonymous <anonymous@ANONYMOUSDEV>
2024-01-03 12:46:01 +0100

    Agregar archivos de proyecto.

---
+++ Promedio/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promedio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3, num4;
+            Console.WriteLine("Introduce el primer número");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduce el segundo número");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduce el tercer número");
+            num3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduce el cuarto número");
+            num4 = Convert.ToInt32(Console.ReadLine());
+            int result = (num1 + num2 + num3 + num4) / 4;
+            Console.WriteLine("El promedio de {0}, {1}, {2}, {3} es: {4}", num1, num2, num3, num4, result);
+            Console.ReadLine();
+        }
+    }
+}
